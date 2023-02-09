b57144bdd82e6d8d1f6fd3ad22bff7b400a180dd
Wighty <53131013+PowerShellYoungTeam@users.noreply.github.com>
2023-02-09 21:41:53 +0000

    Update Find-CrimeUKwithMap.ps1
    Add map zoom and switched to openstreetmap

--- Find-CrimeUKwithMap.ps1
+++ Find-CrimeUKwithMap.ps1
@@ -12,7 +12,7 @@
 .EXAMPLE
    Find-CrimeUK -City Portsmouth -Year 2022 -Month 03
 .EXAMPLE
-   Find-CrimeUK -LocationName Bridgemary -Year 2020 -Month 01
+   Find-CrimeUK -LocationName Bridgemary -Year 2020 -Month 01 -MapZoom 10
 .NOTES
    Information on crime in the UK from the UK Police API
 .COMPONENT
@@ -23,67 +23,95 @@
    Allows you to easily find the crime in your area of the UK via a location name
 #>
 function Find-CrimeUKwithMap {
-    [CmdletBinding()]
-    Param
-    (
-       [Parameter(Mandatory = $true,
-          Position = 0,
-          HelpMessage = "Type the location name of interest such as Gosport or maybe a location in Gosport, like Alverstoke. Keep this to a single word"
-       )]
-       [Alias("Town")]
-       [Alias("City")]
-       [Alias("Village")]
-       [string]$LocationName,
-       [Parameter(Mandatory = $true)]
-       [ValidateSet("2020", "2021", "2022","2023")]
-       [string]$Year,
-       [Parameter(Mandatory = $true)]
-       [ValidateSet("01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11", "12")]
-       [string]$Month
-    )
- 
-    Begin {
-       Write-Verbose -Message "Obtaining the latitude and longitude of the location entered"
-       $place = Invoke-RestMethod "https://geocode.maps.co/search?q={$LocationName}"
-       if ($place.lat.count -gt 1) {
-          $placeLat = $place.lat[0]
-          $placeLon = $place.lon[0]
-       }
-       else {
-          $placeLat = $place.lat
-          $placeLon = $place.lon
-       }
-    }
-    Process {
-       try {
-          $crime = Invoke-RestMethod "https://data.police.uk/api/crimes-street/all-crime?lat=$placeLat&lng=$placeLon&date=$Year-$Month" -ErrorAction Stop
-                   $Props = @()
-          foreach ($item in $crime){
-          $Props += [PSCustomObject]@{
-          Category = $item.category
-          Type = $item.location_type
-          Latitude = $item.location | select -ExpandProperty latitude
-          Longitude = $item.location | select -ExpandProperty longitude
-          Street = ($item.location).street | Select -ExpandProperty name
-          Outcome = ($item.outcome_status).category
-          OutcomeDate = ($item.outcome_status).date
-          Reported = $item.month
-          ID = $item.id
-          PersistentID = $item.persistent_id
-          }
-          }
-         
-        $SelectedProps =  $Props | Out-GridView -PassThru -Title "Crimes Reported - select to view on map" 
 
-        foreach($SelectedProp in $SelectedProps){
-            Start-Process microsoft-edge:"https://www.google.com/maps/@$($SelectedProp.Latitude),$($SelectedProp.Longitude),18.25z"   
-        }
-       }
-       catch {
-          Write-Warning "Crumbs something went wrong most likely an invalid location name $($_)"
-       }
-    }
-    End {
-       Write-Verbose -Message "Script Finished $(Get-Date)"
-    }
- }
+   [CmdletBinding()]
+   Param
+   (
+   [Parameter(Mandatory = $true,
+   Position = 0,
+   HelpMessage = "Type the location name of interest such as Gosport or maybe a location in Gosport, like Alverstoke. Keep this to a single word"
+   )]
+   [Alias("Town")]
+   [Alias("City")]
+   [Alias("Village")]
+   [string]$LocationName,
+   [Parameter(Mandatory = $true)]
+   [ValidateSet("2020", "2021", "2022","2023")]
+   [string]$Year,
+   [Parameter(Mandatory = $true)]
+   [ValidateSet("01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11", "12")]
+   [string]$Month,
+   [int]$MapZoom = 18
+   )
+ 
+   Begin {
+
+      Write-Verbose -Message "Obtaining the latitude and longitude of the location entered"
+
+      $place = Invoke-RestMethod "https://geocode.maps.co/search?q={$LocationName}"
+
+      if ($place.lat.count -gt 1) {
+
+         $placeLat = $place.lat[0]
+         $placeLon = $place.lon[0]
+
+      }else {
+
+         $placeLat = $place.lat
+         $placeLon = $place.lon
+
+      }
+
+   } # End of Begin
+
+   Process {
+
+      try {
+          
+         $crime = Invoke-RestMethod "https://data.police.uk/api/crimes-street/all-crime?lat=$placeLat&lng=$placeLon&date=$Year-$Month" -ErrorAction Stop
+         
+         $Props = @()
+
+         foreach ($item in $crime){
+
+            $Props += [PSCustomObject]@{
+
+               Category = $item.category
+               Type = $item.location_type
+               Latitude = $item.location | select -ExpandProperty latitude
+               Longitude = $item.location | select -ExpandProperty longitude
+               Street = ($item.location).street | Select -ExpandProperty name
+               Outcome = ($item.outcome_status).category
+               OutcomeDate = ($item.outcome_status).date
+               Reported = $item.month
+               ID = $item.id
+               PersistentID = $item.persistent_id
+            }
+
+         }
+         
+         $SelectedProps =  $Props | Out-GridView -PassThru -Title "Crimes Reported - Select to view on map" 
+
+         foreach($SelectedProp in $SelectedProps){ 
+
+            Start-Process microsoft-edge:"https://www.openstreetmap.org/#map=$($MapZoom)/$($SelectedProp.Latitude)/$($SelectedProp.Longitude)" -Verbose -ErrorAction Stop
+
+         }
+
+         }
+
+      catch {
+
+         Write-Warning "Crumbs something went wrong most likely an invalid location name $($_)"
+      
+      }
+
+   } # End of Process
+
+   End {
+
+      Write-Verbose -Message "Script Finished $(Get-Date)"
+
+   } # End of End :)
+
+} # End of Function
